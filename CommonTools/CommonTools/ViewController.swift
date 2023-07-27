610413108fb28d8afc7f849a07379ef237957086
sweetloser <18272160172@163.com>
2023-07-27 13:55:04 +0800

    初始化项目

---
+++ CommonTools/CommonTools/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CommonTools
+//
+//  Created by zengxiangxiang on 2023/7/27.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ CommonTools/Pods/Target Support Files/Pods-CommonToolsTests/Pods-CommonToolsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CommonToolsTests : NSObject
+@end
+@implementation PodsDummy_Pods_CommonToolsTests
+@end
